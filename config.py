dffc6676a27e8d578a323aad774e9d97027c8b83
hishammon2026 <hishammon2026@gmail.com>
2026-04-04 10:03:25 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8717566228:AAEClZPtvCtagIViPRJlhE5Bw2sT6FR-9R8")
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://hishammon:hishammon@cluster0.2g7bqyf.mongodb.net/?appName=Cluster0")
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
-    START_PIC = os.environ.get("START_PIC", "https://graph.org/file/32a61cb5e9dbe98d79f9a-51aea34fedf9627dc6.jpg")
+    START_PIC = os.environ.get("START_PIC", "https://graph.org/file/67914bd37e2baf19ba3c8-8b92823c614fc1c678.jpg")
     ADMINS = [int(admin) for admin in os.environ.get("ADMINS", "7042953166").split()]
